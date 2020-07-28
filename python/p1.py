c974e0f76bc220146026b491721175cf91addea0
varinderjawanda1 <jawandavs1@yahoo.com>
2020-07-28 11:51:26 -0400

    Warm up (if then else)

--- python/p1.py
+++ python/p1.py
@@ -1,4 +1,5 @@
-int1 = 1
+#Ints and concatenation
+int1 = 4
 int2 = 2
 int3 = int1 + int2
 print int3
@@ -8,6 +9,8 @@ int5 = int1 * int2
 print int5
 int6 = int1/int2
 print int6
+
+#strings and concatenation
 
 string1 = 'Varinder'
 print string1
@@ -20,3 +23,23 @@ string4 = 'Jawanda'
 print string4
 # for some reason below print is not printing values
 print 3*("""int3 + int4 + int5 + int6 \nstring3 + string4\n""")
+
+#list and concatenation and nesting
+
+list = [1, 2, 3]
+print list[1]
+newlist = list + [4, 5, 6]
+print newlist[:]
+newlist.append(7)
+print newlist[:]
+newlist2 = [11, 12,13]
+newlist3 = [newlist, newlist2]
+print newlist3[:]
+
+#if then else
+
+if int6 <= 0:
+    print int6
+else:
+    print int2
+
