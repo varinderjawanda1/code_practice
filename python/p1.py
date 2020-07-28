998847d49fb4189605d37c15ed27b14dd481ec0c
varinderjawanda1 <jawandavs1@yahoo.com>
2020-07-28 11:08:10 -0400

    Warm up

--- python/p1.py
+++ python/p1.py
@@ -1,2 +1,21 @@
-var = 2
-print var
+int1 = 1
+int2 = 2
+int3 = int1 + int2
+print int3
+int4 = int1 - int2
+print int4
+int5 = int1 * int2
+print int5
+int6 = int1/int2
+print int6
+
+string1 = 'Varinder'
+print string1
+string2 = 'Jawanda'
+print string2
+print string1 + string2
+string3 = 'Jot'
+print string3
+string4 = 'Jawanda'
+print string4
+print ("""string1 + string2 \nstring3 + string4""")
