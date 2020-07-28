3fb74134945fd8b81726755f5f76e8917e3dab09
varinderjawanda1 <jawandavs1@yahoo.com>
2020-07-28 11:17:12 -0400

    Warm up 2

--- python/p1.py
+++ python/p1.py
@@ -18,4 +18,5 @@ string3 = 'Jot'
 print string3
 string4 = 'Jawanda'
 print string4
-print ("""string1 + string2 \nstring3 + string4""")
+# for some reason below print is not printing values
+print 3*("""int3 + int4 + int5 + int6 \nstring3 + string4\n""")
