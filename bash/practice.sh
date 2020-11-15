0a29c16a931a930828226e80496931294efd26cb
varinderjawanda1 <57777140+varinderjawanda1@users.noreply.github.com>
2020-11-15 17:13:14 -0500

    Update practice.sh

--- bash/practice.sh
+++ bash/practice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 owner=Tom
-host="hostname -i"
-PID="ps -ef |grep bash"
+host=$(hostname -i)
+PID=$(ps -ef |grep bash)
 echo """ Hello ${owner}, This shell is running from ${host} and process ID is : ${PID}"""
