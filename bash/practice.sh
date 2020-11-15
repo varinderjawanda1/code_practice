2af66fbc3e43f33310cad2de1dc2b5e5125f3432
varinderjawanda1 <57777140+varinderjawanda1@users.noreply.github.com>
2020-11-15 17:34:07 -0500

    Update practice.sh

--- bash/practice.sh
+++ bash/practice.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 owner=Tom
 host=$(hostname -i)
-PID=$(ps -ef |grep bash)
+PID=$(echo $$)
 echo """ Hello ${owner}, This shell is running from ${host} and process ID is : ${PID}"""
+
+file_organizer ()
+{
+pwd && ls -lrt
+fileName=$(ls -lrt |test.txt)
+If ${fileName}==test.txt
+  then chmod 777 ${fileName} && ls -lrt
+ else echo """ text.txt doesn't exit"""
+}
+file_organizer ()
+
