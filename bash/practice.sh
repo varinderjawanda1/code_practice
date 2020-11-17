564879cc8db02015fe35d11eb5459e24cbaf86c9
varinderjawanda1 <jawandavs1@yahoo.com>
2020-11-17 08:43:10 -0500

    exercises 6

--- bash/practice.sh
+++ bash/practice.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-owner=Rob
+x=Tom
 host=$(hostname -i)
 PID=$(echo $$)
 
 processVerifier () {
-if [$owner eq Tom] ;
-
-    then echo """ Hello ${owner}, This shell is running from ${host} and process ID is : ${PID}"""
-else
-    echo """Owner is not Tom"""
+if (($x="Tom"))
+   then echo """ Hello ${x}, This shell is running from ${host} and process ID is : ${PID}"""
+   else echo """Owner is not Tom"""
 fi
 }
 processVerifier
